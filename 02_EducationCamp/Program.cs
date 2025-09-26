cd69d2527f564c237882aefaef49d76a13f2f1d8
egem02 <oykuegem@hotmail.com>
2025-09-26 13:49:13 +0300

    Double Variables

---
+++ 02_EducationCamp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_EducationCamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Variables
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            //Console.WriteLine("***** Price List *****");
+            //Console.WriteLine();
+
+            //double applePrice,
+            //       orangePrice,
+            //       strawberryPrice,
+            //       potatoPrice,
+            //       tomatoPrice;
+
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
+
+            //Console.WriteLine("---- Unit Price For Apple: " + applePrice);
+            //Console.WriteLine("---- Unit Price For Orange: " + orangePrice);
+            //Console.WriteLine("---- Unit Price For Strawberry: " + strawberryPrice);
+            //Console.WriteLine("---- Unit Price For Potato: " + potatoPrice);
+            //Console.WriteLine("---- Unit Price For Tomato: " + tomatoPrice);
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.659;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double potatoTotalPrice = potatoGram * potatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+            //Console.WriteLine("Added Product: Apple -  " + "Unit Price: " + applePrice + " - Gram: " + appleGram 
+            //    + " - Total price: " + appleTotalPrice);
+            //Console.WriteLine("Added Product: Orange -  " + "Unit Price: " + orangePrice + " - Gram: " + orangeGram 
+            //    + " - Total price: " + orangeTotalPrice);
+            //Console.WriteLine("Added Product: Strawberry -  " + "Unit Price: " + strawberryPrice + " - Gram: " + strawberryGram 
+            //    + " - Total price: " + strawberryTotalPrice);
+            //Console.WriteLine("Added Product: Potato -  " + "Unit Price: " + potatoPrice + " - Gram: " + potatoGram 
+            //    + " - Total price: " + potatoTotalPrice);
+            //Console.WriteLine("Added Product: tomato -  " + "Unit Price: " + tomatoPrice + " - Gram: " + tomatoGram 
+            //    + " - Total price: " + tomatoTotalPrice);
+          
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice +
+            //    strawberryTotalPrice + potatoTotalPrice  + tomatoPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Shopping Total Budget: " + shoppingTotalPrice + "TL");
+        #endregion
+
+
+            Console.Read();
+
+
+        }
+    }
+}
