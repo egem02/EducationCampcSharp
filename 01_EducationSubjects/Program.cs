2da9092b241355b6df5f360f9df0a7ea1b3b9175
egem02 <oykuegem@hotmail.com>
2025-09-26 09:58:11 +0300

    Add project files.

---
+++ 01_EducationSubjects/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EducationSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.Write("Hello");
+
+            Console.WriteLine("***** Food Categories *****");
+            Console.WriteLine();
+            Console.WriteLine("1-Soups");
+            Console.WriteLine("2-Main Dishes");
+            Console.WriteLine("3-Cold Starters ");
+            Console.WriteLine("4-Salads");
+            Console.WriteLine("5-Deserts");
+            Console.WriteLine("6-Beverages");
+            Console.WriteLine();
+            Console.WriteLine("***** Food Categories *****");
+            Console.Read();
+        }
+    }
+}
+
+//Yazdırma Komutları
