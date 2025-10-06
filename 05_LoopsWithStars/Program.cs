5f27c6ad51dd75b8adf3e2149c182e93e44c7c59
egem02 <oykuegem@hotmail.com>
2025-10-06 12:40:04 +0300

    05 LOOPSwithstars

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region To form ten stars one under the other
+
+            //for (int i = 1; i <= 10; i++) 
+            //{ 
+            //  Console.WriteLine("*");
+
+            //}
+
+
+
+
+
+
+
+            #endregion
+
+            #region To form ten stars one between the other
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+
+            //}
+
+            #endregion
+
+            #region To form ten stars one between the other
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("***********");
+
+            //}
+
+            #endregion
+
+            #region To form stars triangle
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region To form stars triangle (upside down)
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region To form stars diamond
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine(" ");
+            //}
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int j = 1; j <= k; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            
+            
+            #endregion
+           
+
+            Console.Read();
+
+        }
+    }
+}
