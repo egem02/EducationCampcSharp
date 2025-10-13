adac2824674c0f02601dd05e0b4b1d2f6b9749d1
egem02 <oykuegem@hotmail.com>
2025-10-13 11:00:32 +0300

    the last one of the education part 1

---
+++ ClassAndInterface/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndInterface
+{
+    internal class Class1
+    {
+        internal void CallClass()
+        {
+            Console.WriteLine("Class is called");
+        }
+    }
+}
---
+++ ClassAndInterface/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.CallClass();
+            ExampleClass eclass = new ExampleClass("Egem");
+            //eclass.Degisken1 = "Try";
+            //Console.WriteLine(eclass.Degisken1);
+            eclass.Id = 0;
+            eclass.Add();
+            eclass.Update();
+        }
+    }
+    class ExampleClass : TryingClass, MyInterface, IMyInterface2
+    {
+        public ExampleClass(string name)
+        {
+            Degisken1 = name;
+            
+        }
+        public string Degisken1 {  get; set; }
+
+        public void Add()
+        {
+            Console.WriteLine("trying interface method");
+        }
+
+        public void Add2()
+        {
+            Console.WriteLine("Interface trying 2");
+        }
+    }
+
+    public interface MyInterface
+    {
+        //Property ya da method tutuculardır
+        void Add();
+        
+    }
+    public interface IMyInterface2
+    {
+        void Add2();
+    }
+
+    class TryingClass
+    {
+        public int Id { get; set; }
+        public void Update()
+        {
+            Console.WriteLine("update method is working.");
+        }
+    }
+}
